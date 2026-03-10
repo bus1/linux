2428e286ca212eef2eeb6befd96808876671547c
David Rheinsberg <david@readahead.eu>
2026-03-10 11:08:20 +0100

    bus1: parse C API via bindgen
    Provide a bindgen-generated source for the public C API of the bus1 rust
    module. This way, the C API is the authoritative truth of API values,
    and the Rust code can make use of them.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- ipc/bus1/lib.rs
+++ ipc/bus1/lib.rs
@@ -5,3 +5,14 @@
 //! rust. Any user-space API is outside the scope of this module.
 
 use kernel::prelude::*;
+
+#[allow(
+    dead_code,
+    missing_docs,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+)]
+pub mod capi {
+    include!(env!("BUS1_CAPI_PATH"));
+}
