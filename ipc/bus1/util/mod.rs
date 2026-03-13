be1e725cc77f4ec4220b3ae55985d392552fff55
David Rheinsberg <david@readahead.eu>
2026-03-13 09:38:27 +0100

    bus1/util: add Arc conversion helpers
    Add `arc_pin()` and `arc_unpin()` to convert from pinned to unpinned
    Arcs and back.
    
    `Arc` is unconditionally pinned in the kernel. Therefore, most APIs just
    use plain `Arc<T>`, even though they rely on pinning. Unfortunately, any
    generic API that takes a `Pin<T>` cannot accept an `Arc<T>`. Instead,
    one has to emply `unsafe { Pin::new_unchecked(...) }` to get the pinned
    Arc.
    
    Those two helpers allow converting from and to pinned Arcs, allowing
    their use with generic `Pin<T>` APIs without requiring unsafe code.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- ipc/bus1/util/mod.rs
+++ ipc/bus1/util/mod.rs
@@ -4,5 +4,34 @@
 //! This module provides utilities that can be used independently of the core
 //! module.
 
+use kernel::prelude::*;
+use kernel::sync::Arc;
+
 pub mod convert;
 pub mod field;
+
+/// Convert an Arc to its pinned version.
+///
+/// All [`Arc`] instances are unconditionally pinned. It is always safe to
+/// convert from their unpinned variant to their pinned variant.
+///
+/// Most kernel APIs just use a plain [`Arc`], even if they rely on pinning.
+/// If another API needs a `Pin<T>`, this converter can provide it for [`Arc`],
+/// even though most other kernel APIs do not use `Pin<Arc<T>>`.
+pub fn arc_pin<T>(v: Arc<T>) -> Pin<Arc<T>> {
+    // SAFETY: `Arc<T>` guarantees its target is pinned.
+    unsafe { Pin::new_unchecked(v) }
+}
+
+/// Convert an Arc to its unpinned version.
+///
+/// All [`Arc`] instances are unconditionally pinned. It is always safe to
+/// convert from their pinned variant to their unpinned variant.
+///
+/// Most kernel APIs just use a plain [`Arc`], even if they rely on pinning.
+/// This converter allows getting an [`Arc`] if some other API returned a
+/// generic `Pin<T>` with `T = Arc<U>`.
+pub fn arc_unpin<T>(v: Pin<Arc<T>>) -> Arc<T> {
+    // SAFETY: `Arc<T>` guarantees its target is pinned, even if not wrapped.
+    unsafe { Pin::into_inner_unchecked(v) }
+}
