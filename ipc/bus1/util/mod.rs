355a859e0602c3076185a16bfed8a40cb93ee310
David Rheinsberg <david@readahead.eu>
2026-03-19 14:25:31 +0100

    bus1/util: add ptr_addr() and ptr_cmp()
    Add two more generic helpers for dealing with raw pointers. ptr_addr()
    is a backport of `<*mut T>::addr()`, and `ptr_cmp()` is similar to
    `core::ptr::eq()`, but for ordering instead of equality.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- ipc/bus1/util/mod.rs
+++ ipc/bus1/util/mod.rs
@@ -52,3 +52,31 @@ pub fn nonnull_from_mut<T: ?Sized>(v: &mut T) -> core::ptr::NonNull<T> {
     // SAFETY: A reference cannot be NULL.
     unsafe { core::ptr::NonNull::new_unchecked(v) }
 }
+
+/// Return the memory address part of a pointer without exposing provenance.
+///
+/// This returns the same value as an `as usize` cast. However, this function
+/// is meant to not expose provenance, and rather behave like
+/// `<*mut T>::addr()`. Unfortunately, the latter requires an MSRV of 1.84,
+/// which is not yet available upstream. Until then, this serves as a
+/// replacement.
+pub fn ptr_addr<T: ?Sized>(v: *const T) -> usize {
+    // Simply expose the provenance until. A transmute would avoid the
+    // exposition, but is not a stable API.
+    v.cast::<()>() as usize
+}
+
+/// Compare two pointers.
+///
+/// This is equivalent to `<*const T as Ord>::cmp()`. Unlike the trait-based
+/// solution, this has fixed pointer types and thus can be called with
+/// references, which are then coerced to pointers.
+///
+/// This serves the same purpose as `core::ptr::eq()`, but for `Ord` rather
+/// than `Eq`.
+pub fn ptr_cmp<T: ?Sized>(a: *const T, b: *const T) -> core::cmp::Ordering {
+    // Even though `PartialOrd for *mut T` documents that it uses
+    // `<*mut T>::addr()` for comparisons, clippy still warns about it. Cast
+    // to `()` to ensure metadata is ignored.
+    a.cast::<()>().cmp(&b.cast::<()>())
+}
